5f473233ce18927f124523a19b7c8a6518ad93d4
Rob Ballantyne <rob@dynamedia.uk>
2022-06-06 12:17:09 +0100

    ...

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -1,43 +1,8 @@
-$(function() {
-  // owl carousel script starts
-  if ($("#main-banner-carousel").length) {
-    $("#main-banner-carousel").owlCarousel({
-      loop: true,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplaySpeed: 2000,
-      autoplayHoverPause: true,
-      autoWidth: false,
-      dots: true,
-      margin: 0,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        320: {
-          items: 1
-        }
-      }
-    });
-  }
 
-  // scroll header script here
-  window.onscroll = function() {
-    scrollHeader();
-  };
-  // Get the header
-  var header = $(".navbar-bottom-menu");
-  var body = $("body");
-  function scrollHeader() {
-    // adding sticky class
-    if (window.pageYOffset > 105) {
-      $(header).addClass("sticky");
-    } else {
-      // removing sticky class
-      $(header).removeClass("sticky");
-    }
-  }
+
+
+$(function() {
+
 
   // navbar toggler script
   $(".navbar-toggler").on("click", function() {
